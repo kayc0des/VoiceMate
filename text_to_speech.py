4a682ce55f3770afe2d56529cd7a10a84e29c2be
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2023-08-02 08:20:59 +0200

    text to speech moved to voicemate directory

--- text_to_speech.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-
-engine = pyttsx3.init()
-
-def respond(response_text):
-    # Function to generate speech output for responses
-    print(response_text)
-    engine.say(response_text)
-    engine.runAndWait()
