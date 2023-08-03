16475b0d60edd1caaa7a91ffd6dfe9019386b9cf
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2023-08-03 21:24:32 +0200

    gTTS added

--- voicemate/text_to_speech.py
+++ voicemate/text_to_speech.py
@@ -1,9 +1,9 @@
-import pyttsx3
-
-engine = pyttsx3.init()
+from gtts import gTTS
+from playsound import playsound
 
 def respond(response_text):
     # Function to generate speech output for responses
     print(response_text)
-    engine.say(response_text)
-    engine.runAndWait()
+    tts = gTTS(text=response_text, lang='en')
+    tts.save("response.mp3")
+    playsound("response.mp3")
